aed0f6155cb434470e3e61eace2c7d0ac08ca6b6
rlorimier <rere.rnt@gmail.com>
2022-03-29 09:56:41 +0100

    add a list with hangmans

--- hangman.py
+++ hangman.py
@@ -1,8 +1,45 @@
 import random
 
-# main variables
-hangmans = None
+# selecting the word from a list
 words_list = "squirrel bear whale coyote hedgehog lion dolphin crocodile raccoon hyena monkey panda deer leopard kangaroo tiger zebra giraffe hippo wolf elephant gorilla snake eagle antelope vulture panther parrot rhino shark rabbit reindeer lizard leopard koala frog turtle toucan spider sparrow scorpion moose iguana capybara butterfly bison raven falcon sheep buffalo wildebeest baboon ostrich flamingo jackal".upper().split()
+word = random.choice(words_list)
 
-word_choice = random.choice(words_list)
-print(word_choice)
+# list with hangmans
+hangmans = ['''
+   +---+
+       |
+       |
+       |
+      ===''', '''
+   +---+
+   O   |
+       |
+       |
+      ===''', '''
+   +---+
+   O   |
+   |   |
+       |
+      ===''', '''
+   +---+
+   O   |
+  /|   |
+       |
+      ===''', '''
+   +---+
+   O   |
+  /|\  |
+       |
+      ===''', '''
+   +---+
+   O   |
+  /|\  |
+  /    |
+      ===''', '''
+   +---+
+   O   |
+  /|\  |
+  / \  |
+      ===''']
+
+
